8df3de0437f056ce0bb0d51e7cf047d1340d2238
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-22 20:53:00 +0800

    配置API，添加key

---
+++ src/api.js
@@ -0,0 +1,39 @@
+//base URL
+const base_url = "https://api.rawg.io/api/";
+// API KEY
+const apiKey = "66b04e46d2b44d50a48e4400b9b9e6ad";
+
+// 获取当前月份
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  if (month < 10) return `0${month}`;
+  return month;
+};
+
+// 获取当前日期
+const getCurrentDay = () => {
+  const day = new Date().getDate();
+  if (day < 10) return `0${day}`;
+  return day;
+};
+
+// 当前日期 year-month-day
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// 流行的游戏
+const popularGames = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcomingGames = `games?key=${apiKey}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${apiKey}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+
+const popularGamesURL = () => `${base_url}${popularGames}`;
+const upcomingGamesURL = () => `${base_url}${upcomingGames}`;
+const newGamesURL = () => `${base_url}${newGames}`;
+
+// 游戏详情
+
+export { popularGamesURL, upcomingGamesURL, newGamesURL };
