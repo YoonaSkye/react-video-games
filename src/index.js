9c0010813b2bac4549364f5666dfe2fd682a2433
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-02-22 21:14:50 +0800

    配置redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// 配置Redux
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+
+import rootReducer from "./reducer/rootReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware()));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
